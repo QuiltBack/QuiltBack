0ecc617adc32bffefc7f85a42a8213f040d21f61
Garrett Swensen <garrettvaughnswensen@gmail.com>
2017-09-25 14:16:09 -0600

    with link to Login added for testing backend

---
+++ app/src/components/home/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default HomePage;
