fa3e10bc62c15400262d61ae81198a0ac8e15fc7
Fernando De la Vega <fernando.delavega2847@gcssk12.net>
2017-09-26 17:34:11 -0600

    added the footer.

--- app/src/components/common/Footer.js
+++ app/src/components/common/Footer.js
@@ -4,7 +4,25 @@ import '../../styles/Footer.css';
 const Footer = () => {
   return (
     <footer className='main-footer-container'>
-      test
+      <div className='footer-grid-one'>
+        <div className='footer-question'>Have a question? Comment?</div>
+        <button className='footer-contact'>CONTACT US</button>
+        <div className='footer-copyright'>QuiltBack is a 501(c)(3) not-for-profit organization. Copyright QuiltBack, 2017.</div>
+      </div>
+      <div className='footer-grid-two'>
+        <div className='footer-stay-connected'>Stay Connected</div>
+        <div className='footer-receive-emails'>Receive e-mails when a new event is hosted or a new blog posted.</div>
+        <form className='footer-form'>
+          <input className='footer-email-address' placeholder='Email Address'/>
+          <button className='footer-button-subscribe'>SUBSCRIBE</button>
+        </form>
+      </div>
+      <div className='footer-grid-three'>
+        <div className='footer-follow-us'>Follow Us</div>
+        <div className='footer-facebook'>F</div>
+        <div className='footer-twitter'>T</div>
+        <div className='footer-google'>G</div>
+      </div>
     </footer>  
   )
 }
