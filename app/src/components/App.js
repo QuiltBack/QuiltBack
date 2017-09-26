7f4192dbeaf503d3c73543e5c0ee3415017c0dbe
Garrett Swensen <garrettvaughnswensen@gmail.com>
2017-09-26 10:18:05 -0600

    with css laoder and otf font files

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import Login from './Login/';
+import Login from './login/Login';
+import Header from './common/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header/>
         <Login/>
       </div>
     );
---
+++ app/src/webpack.config.dev.js
@@ -0,0 +1,14 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
+        },
+        {
+          loader: 'css-loader',
+          options: { root: '.' }
+        }
+      ]
+    }
+}
