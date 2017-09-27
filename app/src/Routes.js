1a8a86cbff63ab6a48b6d0a6b0a47742aae62234
Garrett Swensen <garrettvaughnswensen@gmail.com>
2017-09-27 12:27:40 -0600

    main banner layout complete

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -3,10 +3,14 @@ import React from 'react';
 import ApiTest from './components/apiTest/ApiTest';
 import ApiPosts from './components/apiTest/ApiPosts';
 import ApiEvents from './components/apiTest/ApiEvents';
+import HomePage from './components/home/HomePage';
 
 export default (
     <Switch>
+        <Route exact path="/home" component={HomePage} >
+        </Route> 
         <Route path="/apitest" component={ApiTest} >
-       </Route>
+        </Route>
+        
     </Switch>
 );
--- app/src/components/home/HomePage.js
+++ app/src/components/home/HomePage.js
@@ -5,7 +5,47 @@ class HomePage extends React.Component {
     render() {
         return (
             <div>
-                
+                <span className="banner-container">
+                    <svg  className="quilt-back-full-logo-site" width="193px" height="193px" viewBox="0 0 193 193" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                            <g id="HOME" transform="translate(-262.000000, -178.000000)">
+                                <g id="Banner-Image">
+                                    <g id="QuiltBack---Full-Logo-Site" transform="translate(262.000000, 178.000000)">
+                                        <rect id="Rectangle-3" fill="#D93044" fill-rule="evenodd" x="64.3333333" y="64.3333333" width="64.3333333" height="64.3333333"></rect>
+                                        <text id="QUILT" font-family="BloggerSans-Bold, Blogger Sans" font-size="23.3208333" font-style="condensed" font-weight="bold" fill="#FFFFFF">
+                                            <tspan x="67.7192771" y="91.1791667">QUILT</tspan>
+                                        </text>
+                                        <text id="BACK" font-family="BloggerSans-Bold, Blogger Sans" font-size="23.3208333" font-style="condensed" font-weight="bold" letter-spacing="1.4474999" fill="#FFFFFF">
+                                            <tspan x="67.6207668" y="118.520833">BAC</tspan>
+                                            <tspan x="111.608683" y="118.520833">K</tspan>
+                                        </text>
+                                        <g id="Group" stroke-width="1" fill-rule="evenodd" transform="translate(64.333333, 0.000000)" fill="#D93044">
+                                            <polygon id="Triangle" transform="translate(16.083333, 32.166667) rotate(90.000000) translate(-16.083333, -32.166667) " points="16.0833333 16.0833333 48.25 48.25 -16.0833333 48.25"></polygon>
+                                            <polygon id="Triangle" transform="translate(48.250000, 32.166667) rotate(-90.000000) translate(-48.250000, -32.166667) " points="48.25 16.0833333 80.4166667 48.25 16.0833333 48.25"></polygon>
+                                        </g>
+                                        <g id="Group" stroke-width="1" fill-rule="evenodd" transform="translate(160.833333, 96.500000) rotate(90.000000) translate(-160.833333, -96.500000) translate(128.666667, 64.333333)" fill="#D93044">
+                                            <polygon id="Triangle" transform="translate(16.083333, 32.166667) rotate(90.000000) translate(-16.083333, -32.166667) " points="16.0833333 16.0833333 48.25 48.25 -16.0833333 48.25"></polygon>
+                                            <polygon id="Triangle" transform="translate(48.250000, 32.166667) rotate(-90.000000) translate(-48.250000, -32.166667) " points="48.25 16.0833333 80.4166667 48.25 16.0833333 48.25"></polygon>
+                                        </g>
+                                        <g id="Group" stroke-width="1" fill-rule="evenodd" transform="translate(32.166667, 96.500000) rotate(90.000000) translate(-32.166667, -96.500000) translate(0.000000, 64.333333)" fill="#D93044">
+                                            <polygon id="Triangle" transform="translate(16.083333, 32.166667) rotate(90.000000) translate(-16.083333, -32.166667) " points="16.0833333 16.0833333 48.25 48.25 -16.0833333 48.25"></polygon>
+                                            <polygon id="Triangle" transform="translate(48.250000, 32.166667) rotate(-90.000000) translate(-48.250000, -32.166667) " points="48.25 16.0833333 80.4166667 48.25 16.0833333 48.25"></polygon>
+                                        </g>
+                                        <g id="Group" stroke-width="1" fill-rule="evenodd" transform="translate(64.333333, 128.666667)" fill="#D93044">
+                                            <polygon id="Triangle" transform="translate(16.083333, 32.166667) rotate(90.000000) translate(-16.083333, -32.166667) " points="16.0833333 16.0833333 48.25 48.25 -16.0833333 48.25"></polygon>
+                                            <polygon id="Triangle" transform="translate(48.250000, 32.166667) rotate(-90.000000) translate(-48.250000, -32.166667) " points="48.25 16.0833333 80.4166667 48.25 16.0833333 48.25"></polygon>
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                    <div className="section_b">
+                        <h1 className="quilt-back-gives-back">QuiltBack Gives Back</h1>
+                        <div className="rectangle-2"></div>
+                        <p className="we-are-a-community">We are a community that fosters aid for refugees by hosting charity quilt auctions. Learn how you can get involved. Become a QuiltBacker today.</p>
+                    </div>
+                </span>
             </div>
         )
     }
