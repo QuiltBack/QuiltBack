6a68d1b815f6ac3c8d7cb294a1ef5463e7b71b01
Jack Weinrauch <jack.weinrauch@aggiemail.usu.edu>
2017-09-28 14:28:02 -0600

    updated some events files

--- app/server/db/createEventsTable.sql
+++ app/server/db/createEventsTable.sql
@@ -1,7 +1,10 @@
 CREATE TABLE events(
 Date DATE,
 Time VARCHAR(50),
-Location TEXT,
+City TEXT,
+State TEXT,
+Zipcode INT,
+Address TEXT,
 Title VARCHAR(50),
 Description TEXT,
 ImageRef VARCHAR(50),
